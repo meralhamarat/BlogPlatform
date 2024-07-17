1f08c08e40ecd2b828d69aa217055b56d0dda07b
Meral Hamarat <meralhamarat24@gmail.com>
2024-07-17 15:15:17 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace BlogPlatform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.buttonAddPost = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sqliteCommand1 = new Microsoft.Data.Sqlite.SqliteCommand();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(51, 65);
+            this.textBoxTitle.Multiline = true;
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(251, 271);
+            this.textBoxTitle.TabIndex = 0;
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Location = new System.Drawing.Point(51, 26);
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(251, 22);
+            this.textBoxContent.TabIndex = 1;
+            this.textBoxContent.TextChanged += new System.EventHandler(this.textBoxContent_TextChanged);
+            // 
+            // buttonAddPost
+            // 
+            this.buttonAddPost.Location = new System.Drawing.Point(334, 26);
+            this.buttonAddPost.Name = "buttonAddPost";
+            this.buttonAddPost.Size = new System.Drawing.Size(107, 22);
+            this.buttonAddPost.TabIndex = 2;
+            this.buttonAddPost.Text = "button1";
+            this.buttonAddPost.UseVisualStyleBackColor = true;
+            this.buttonAddPost.Click += new System.EventHandler(this.buttonAddPost_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(325, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // sqliteCommand1
+            // 
+            this.sqliteCommand1.CommandTimeout = 30;
+            this.sqliteCommand1.Connection = null;
+            this.sqliteCommand1.Transaction = null;
+            this.sqliteCommand1.UpdatedRowSource = System.Data.UpdateRowSource.None;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonAddPost);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.textBoxTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void buttonAddPost_Click_1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void textBoxContent_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.Button buttonAddPost;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Microsoft.Data.Sqlite.SqliteCommand sqliteCommand1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+using System.Windows.Forms;
+
+namespace BlogPlatform
+{
+    public partial class Form1 : Form
+    {
+        private SqliteConnection sqliteConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            LoadBlogPosts();
+        }
+
+        private void InitializeDatabase()
+        {
+            sqliteConnection = new SqliteConnection("Data Source=blog.db;");
+            sqliteConnection.Open();
+
+            string createTableQuery = "CREATE TABLE IF NOT EXISTS BlogPosts (Id INTEGER PRIMARY KEY, Title TEXT, Content TEXT)";
+            using (SqliteCommand command = new SqliteCommand(createTableQuery, sqliteConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void LoadBlogPosts()
+        {
+            string selectQuery = "SELECT * FROM BlogPosts";
+            using (SqliteCommand command = new SqliteCommand(selectQuery, sqliteConnection))
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void buttonAddPost_Click(object sender, EventArgs e)
+        {
+            string title = textBoxTitle.Text;
+            string content = textBoxContent.Text;
+
+            string insertQuery = "INSERT INTO BlogPosts (Title, Content) VALUES (@title, @content)";
+            using (SqliteCommand command = new SqliteCommand(insertQuery, sqliteConnection))
+            {
+                command.Parameters.AddWithValue("@title", title);
+                command.Parameters.AddWithValue("@content", content);
+                command.ExecuteNonQuery();
+            }
+
+            LoadBlogPosts();
+
+            textBoxTitle.Clear();
+            textBoxContent.Clear();
+        }
+    }
+}
